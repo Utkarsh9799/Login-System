d8fb5367e272a1593814bd5f2facc5fe3530ea1f
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-18 00:58:23 +0530

    Added dependencies

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
