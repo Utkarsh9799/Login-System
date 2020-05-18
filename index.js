ddf5a936d99e821d65c1bacb4b1cc6a51ee24901
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-18 17:28:21 +0530

    Added home page, login and register views

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 //Initialising app
 const app = express();
 
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
 //Routes
 app.use('/', require('./routes/home'));
+app.use('/users', require('./routes/users'));
+
 
 const PORT = process.env.PORT || 5000;
 
