37f30ac76b548c43347167cab2a15e965a4d7abf
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-19 17:59:03 +0530

    Database connectivity with password encryption

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//User model
+const User = require('../models/User');
 
 //Login page
 router.get('/login', (req, res) => res.render("login"));
@@ -35,7 +39,44 @@ router.post('/register', (req, res) => {
     else
     {
         //If all fields have valid entry    
-        
+        User.findOne({email: email})
+            .then(user => {
+                if(user) 
+                {
+                    errors.push({msg: 'Email is already registered!'});
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                }
+                else
+                {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+                    
+                    //Encrypting password
+                    bcrypt.genSalt(10, (err, salt) => 
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err) throw err;
+
+                            //Setting password as hash value
+                            newUser.password = hash;
+
+                            //Saving user to database
+                            newUser.save()
+                                .then(user => {
+                                    res.render('/users/login');
+                                })
+                                .catch(err => console.log(err));
+                        }))
+                }
+            });
     }
 });
 
