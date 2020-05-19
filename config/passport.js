d11edc565b778f3618de6d61cef4f62496574c0b
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-20 02:43:39 +0530

    Created local strategy for authentication

---
+++ config/passport.js
@@ -0,0 +1,32 @@
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/User');
+
+module.exports = function(passport) {
+    passport.use(
+        new LocalStrategy({usernameField: 'email'}, (email, password, done) => {
+            //Checking if user exists using email
+            User.findOne({email: email})
+                .then(user => {
+                    if(!user) {
+                        return done(null, false, {message: 'Email is not registered!'});
+                    }
+
+                    //If user exists, check password
+                    bcrypt.compare(password, user.password, (err, success) => {
+                        if(err) throw err;
+
+                        if(success) {
+                            return done(null, user);
+                        }
+                        else {
+                            return done(null, false, {message: 'Incorrect password!'});
+                        }
+                    });
+                })
+                .catch(err => console.log(err));
+        })
+    );
+}
